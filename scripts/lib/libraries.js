aceaef367ddc5138c2733c7134c8236072b73782
yokobond <koji.yokokawa@gmail.com>
2024-04-30 09:24:43 +0900

    feat: make it PWA for scratch-gui v4
    refactor: lint

---
+++ scripts/lib/libraries.js
@@ -0,0 +1,13 @@
+const backdrops = require('../../src/lib/libraries/backdrops.json');
+const costumes = require('../../src/lib/libraries/costumes.json');
+const sounds = require('../../src/lib/libraries/sounds.json');
+const sprites = require('../../src/lib/libraries/sprites.json');
+
+const libraries = {
+    backdrops,
+    costumes,
+    sounds,
+    sprites
+};
+
+module.exports = libraries;
