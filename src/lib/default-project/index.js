79e72197a52e66b1811cea03971660ff4a818389
yokobond <koji.yokokawa@gmail.com>
2024-05-03 20:37:07 +0900

    feat: replace costume of the default project

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -4,8 +4,8 @@ import projectData from './project-data';
 import popWav from '!arraybuffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav?';
 import meowWav from '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav?';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg?';
-import costume1 from '!raw-loader!./bcf454acf82e4504149f7ffe07081dbc.svg?';
-import costume2 from '!raw-loader!./0fb9be3e8397c983338cb71dc84d0b25.svg?';
+import costume1 from '!raw-loader!./881c5994e99006fc05359d738af66337.svg?';
+import costume2 from '!raw-loader!./b6e71f1bf154c8a5470665fe87dd507c.svg?';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -39,12 +39,12 @@ const defaultProject = translator => {
         dataFormat: 'SVG',
         data: encoder.encode(backdrop)
     }, {
-        id: 'bcf454acf82e4504149f7ffe07081dbc',
+        id: '881c5994e99006fc05359d738af66337',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume1)
     }, {
-        id: '0fb9be3e8397c983338cb71dc84d0b25',
+        id: 'b6e71f1bf154c8a5470665fe87dd507c',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume2)
